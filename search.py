b8c081dbf0ed36d2c5643ddc7e2dd7b598f5ca57
alpha <peker.celik@ozu.edu.tr>
2023-07-13 01:55:29 +0300

    Rabbit mq publishing function implemented to the search file

--- search.py
+++ search.py
@@ -1,45 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from urllib.parse import unquote
+import pika
 
 def search_shopify_stores(query, num_results=10):
     search_url = f"https://www.google.com/search?q={query}+site:.store"
-    output_file = "urls.txt"
-
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='shopify')
     try:
         response = requests.get(search_url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "html.parser")
-            search_results = soup.select("div.g")
+            search_results = soup.select("a")
             print(f"Search results for '{query}':")
-            with open(output_file, "w") as file:
-                count = 0
-                for result in search_results:
-                    link_element = result.select_one("a")
-                    if link_element:
-                        link = link_element["href"]
-                        store_url = extract_shopify_store_url(link)
-                        if store_url:
-                            count += 1
-                            print(f"Result {count}:")
-                            print(f"Store URL: {store_url}")
-                            file.write(store_url + "\n")
-                            print("-" * 20)
-                            if count >= num_results:
-                                break
+            for result in search_results:
+                link = result.get("href")
+                if link and link.startswith("/url?q="):
+                    store_url = extract_shopify_store_url(link)
+                    if store_url:
+                        count += 1
+                        print(f"Result {count}:")
+                        print(f"Store URL: {store_url}")
+                        channel.basic_publish(exchange='',
+                        routing_key='shopify',
+                        body=store_url)
+                        print("-" * 20)
+                        if count >= num_results:
+                            connection.close()
+                            break
             print(f"Shopify store URLs saved to '{output_file}'.")
         else:
             print(f"Failed to search Shopify stores. Error: {response.text}")
     except requests.exceptions.RequestException as e:
         print(f"Error occurred while sending the request: {e}")
 
-def extract_shopify_store_url(google_link):
-    match = re.search(r"https?://([^/?]+).*", google_link)
+def extract_shopify_store_url(link):
+    match = re.search(r"/url\?q=(https?://[^&]+)", link)
     if match:
-        return match.group(0)
+        url = match.group(1)
+        return unquote(url)
     return None
 
 # Example usage
 search_query = "fashion"  # The query to search Shopify stores
-num_results = 5  # Number of results to retrieve (adjust as needed)
+num_results = 5000  # Number of results to retrieve (adjust as needed)
 search_shopify_stores(search_query, num_results)
